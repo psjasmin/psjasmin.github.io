77e4d7d791aaf451c248f1acb3969a6093a727f2
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2023-05-29 16:45:01 +0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// MAIN JAVASCRIPT FILE
+(function ($) {
+    "use strict";
+
+    // Spinner (Loading Screen)
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wow js
+    new WOW().init();
+
+
+    // Fixed Navigational Bar
+    $('.fixed-top').css('top', $('.top-bar').height());
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('.fixed-top').addClass('bg-dark').css('top', 0);
+        } else {
+            $('.fixed-top').removeClass('bg-dark').css('top', $('.top-bar').height());
+        }
+    });
+    
+    
+    // Back to top Button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header Carousel (Home Page-Index)
+    $(".header-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        loop: true,
+        nav: true,
+        dots: false,
+        items: 1,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>', //left button
+            '<i class="bi bi-chevron-right"></i>' //right button
+        ]
+    });
+
+
+    // Number Counter (About Page)
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Game Reviews Carousel (Home Page-Index)
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>', //left button
+            '<i class="bi bi-chevron-right"></i>' //right button
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+})(jQuery);
